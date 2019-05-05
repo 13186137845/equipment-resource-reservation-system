effd875c1a612263f8f9f9acebf15b6ed9d551f3
czw <1500690094@qq.com>
2019-05-05 15:18:29 +0800

    登录注册项目结构

---
+++ 02-sourse/ERRS/src/components/LoginAndRegister/Register.vue
@@ -0,0 +1,23 @@
+<template>
+  <div>
+    <h1>{{ msg }}</h1>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'register',
+  data () {
+    return {
+      msg: '注册'
+    }
+  }
+}
+</script>
+
+<style scoped>
+h2 {
+  font-weight: normal;
+}
+
+</style>
--- 02-sourse/ERRS/src/router/index.js
+++ 02-sourse/ERRS/src/router/index.js
@@ -1,15 +1,18 @@
 import Vue from 'vue'
 import Router from 'vue-router'
 import HelloWorld from '@/components/HelloWorld'
+import Register from '@/components/LoginAndRegister/Register'
 
 Vue.use(Router)
 
 export default new Router({
-  routes: [
-    {
-      path: '/',
-      name: 'HelloWorld',
-      component: HelloWorld
-    }
-  ]
+    routes: [{
+        path: '/',
+        name: 'HelloWorld',
+        component: HelloWorld
+    }, {
+        path: '/register',
+        name: 'Register',
+        component: Register
+    }]
 })
