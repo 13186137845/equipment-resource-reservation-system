733cd3b850004ed7076f06bea47ba96d4ceaf6fc
czw <1500690094@qq.com>
2019-05-17 09:00:46 +0800

    相关依赖文件

---
+++ 02-sourse/ERRS/src/common.1/api.js
@@ -0,0 +1,98 @@
+import request from '@/plugin/axios'
+
+/**
+ * @description [ sys ] 登陆注销相关
+ */
+export const sysAccountService = {
+  /**
+   * 登录
+   */
+  login (data) {
+    return request({
+      url: '/login',
+      method: 'post',
+      data
+    })
+  }
+}
+
+/**
+ * @description [ sys ] 系统信息
+ */
+export const sysInfoService = {
+  /**
+   * 获取系统信息
+   */
+  get () {
+    return request({
+      url: '/sys/info'
+    })
+  }
+}
+
+/**
+ * @description [ sys ] 菜单和路由相关
+ */
+export const sysMenuService = {
+  /**
+   * 获取菜单
+   */
+  getNav () {
+    return request({
+      url: '/sys/menu/nav'
+    })
+  },
+  /**
+   * 获取权限
+   */
+  getPermissions () {
+    return request({
+      url: '/sys/menu/permissions'
+    })
+  }
+}
+
+/**
+ * @description [ admin ] 管理员后台管理相关
+ */
+export const adminUserService = {
+
+  //获取系统用户信息
+  getInfo () {
+    return request({
+      url: 'adminApi/v/userList',
+      method:'post'
+    })
+  },
+  //添加用户信息
+  addUser(data){
+    return request({
+      url:'adminApi/i/insUser',
+      method:'put',
+      data
+    })
+  },
+  //获取添加用户信息下拉列表框信息
+  getAddUserMenu(){
+    return request({
+      url:'adminApi/v/userInsView',
+      method:'get'
+    })
+  },
+  //修改用户信息
+  updateUser(data){
+    return request({
+      url:'adminApi/u/updUser',
+      method:'post',
+      data
+    })
+  },
+  //修改用户密码
+  updatePassword (data) {
+    return request({
+      url: '',
+      method: '',
+      data
+    })
+  }
+}
